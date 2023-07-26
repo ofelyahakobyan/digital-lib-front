1b44c203d5ce252e64041b6d3912339cc918d274
ofelya <ofelyahakobyan2@gmail.com>
2023-07-26 08:30:23 +0400

    sample image on the bestsellers page, should be deleted further

--- src/pages/Bestseller.js
+++ src/pages/Bestseller.js
@@ -6,6 +6,10 @@ function Bestseller() {
     <Wrapper>
       <div style={{ height: 500, backgroundColor: 'white', paddingTop: 100 }}>
         <p> BestsellerPage </p>
+        {/* <img
+          src="http://localhost:4000/images\authors\small-author-paulo-77b5f51b-d3a3-4d5c-86cf-dde4c0517d97_paolo.jpg"
+          alt="author"
+        /> */}
       </div>
     </Wrapper>
   );
